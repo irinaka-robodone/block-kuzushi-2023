c1172b2565ce31cba695a7dc55cf15240b857db8
natsu <70moonatu38@gmail.com>
2024-02-28 20:37:45 +0900

    ball angle

--- src/app.py
+++ src/app.py
@@ -4,13 +4,12 @@ import pyxel
 
 class App:
     def __init__(self):
-        self.SCREEN_SIZE = (175, 120)
+        self.SCREEN_SIZE = (200,240)
         self.FPS = 60
         pyxel.init(self.SCREEN_SIZE[0], self.SCREEN_SIZE[1], title="ブロック崩し", fps=self.FPS)
         # pyxel.images[0].load(0, 0, "assets/pyxel_logo_38x16.png")
-        
         self.reset()
-        self.bar_disappeared_count = 0
+        
         pyxel.run(self.update, self.draw)
         
     def reset(self):
@@ -19,18 +18,20 @@ class App:
         self.create_bar()
         self.create_ball()
         self.create_rects()
+        self.bar_disappeared_count = 0
+        self.score = 0
 
     def create_bar(self):
         self.x = 75
-        self.y = 100
-        self.width = 50
-        self.height = 3
+        self.y = 200
+        self.width = 200
+        self.height = 5
     
     def create_ball(self):
-        ball_x = 25
+        ball_x = 100
         ball_y = 40
-        ball_radius = 2.5 
-        speed = 0.5
+        ball_radius = 5
+        speed = 1.5
         ball_dx = 1
         ball_dy = 1
         self.balls.append({"x": ball_x, "y": ball_y, "dx": ball_dx, "dy": ball_dy, "radius": ball_radius, "speed": speed})
@@ -54,23 +55,26 @@ class App:
             rect_height = 5
             self.rectangles.append({"x": rect_x, "y": rect_y, "width": rect_width, "height": rect_height})
     
+    
+    
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+        
         self.update_bar()
         self.update_ball()
         self.update_gameover()
         self.update_rects()
-        if pyxel.frame_count % (60 * 20) == 0:
+        
+        if pyxel.frame_count % (60 * 120) == 0:
             self.create_rects()
-        if pyxel.frame_count % (60 * 60) == 0 and pyxel.frame_count % (60 * 60)>0:
-            self.create_ball()
+        
         
     def update_bar(self):
         if pyxel.btn(pyxel.KEY_RIGHT):
-            self.x += 1.5
+            self.x += 2.0
         if pyxel.btn(pyxel.KEY_LEFT):
-            self.x -= 1.5
+            self.x -= 2.0
         
     def update_ball(self):
         for ball in self.balls:
@@ -84,15 +88,18 @@ class App:
             # ボールが壁に当たった場合、反転
             if ball["x"] - ball["radius"] < 0 or ball["x"] + ball["radius"] > pyxel.width:
                 ball["dx"] = -ball["dx"]
+                ball["dy"] *= 1.1 
             # ボール壁に当たった場合、反転
             if ball["y"] - ball["radius"] < 0:
                 ball["dy"] = -ball["dy"]  
         # update_gameover メソッドを呼び出す
     def update_gameover(self):
         for ball in self.balls:
-            if all(ball["y"] + ball["radius"] > self.SCREEN_SIZE[1] - 10 for ball in self.balls):
+            if all(ball["y"] + ball["radius"] > self.SCREEN_SIZE[1]  for ball in self.balls):
+                
                 pyxel.quit()       
         # update_rects(self) メソッドを呼び出す
+    #短形ブロック
     def update_rects(self):      
         for ball in self.balls:
             for rect in self.rectangles:
@@ -100,13 +107,24 @@ class App:
                     self.rectangles.remove(rect)
                     ball["dy"] = -ball["dy"]
                     self.bar_disappeared_count += 1
+                    self.score += 1
+        if self.bar_disappeared_count % 5 == 0 and self.bar_disappeared_count >0:
+            self.create_ball()
+            self.bar_disappeared_count = 0
+        
+            
+            
+
+        
         # 描画する処理
     def draw(self):
         pyxel.cls(0)
         self.draw_bar()
         self.draw_ball()
         self.draw_rects()
-        pyxel.text(10,10, f"Points: {self.bar_disappeared_count}",7)
+        pyxel.text(10,10, f"Points: {self.score}",7)
+        
+        
         # 長方形を描画する処理
     def draw_bar(self):
         pyxel.rect(self.x, self.y, self.width, self.height, 5)
@@ -119,5 +137,9 @@ class App:
         for rect in self.rectangles:
             if 0 <= rect["x"] <= self.SCREEN_SIZE[0] and 0 <= rect["y"] <= self.SCREEN_SIZE[1]:
                 pyxel.rect(rect["x"], rect["y"], rect["width"], rect["height"],12)
-
+    def draw_clear_screen(self):
+        if self.is_game_clear():
+            pyxel.text(self.SCREEN_SIZE[0] // 2 - 40, self.SCREEN_SIZE[1] // 2, "Game Clear!", 7)
+            pyxel.text(self.SCREEN_SIZE[0] // 2 - 60, self.SCREEN_SIZE[1] // 2 + 10, "Press 'Q' to quit", 7)
+        
 App()
